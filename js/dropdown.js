e1ee5824d3bdfef4b5652c0324e5d2472d7097c8
PabloSKZ <pablo.eskenazi@gmail.Com>
2020-10-20 11:50:09 +0200

    :sparkles: sort by popularity and title

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,11 +1,11 @@
 /* DOM Variables */
 const $dropdownButton = document.getElementById("dropdown-button");
-const $selected = document.getElementById("dropdown-selected");
-const $list = document.getElementById("dropdown-list");
-const $arrow = document.getElementById("dropdown-arrow");
-const $popularity = document.getElementById("dropdown-list-popularity");
-const $date = document.getElementById("dropdown-list-date");
-const $title = document.getElementById("dropdown-list-title");
+const $dropdownSelected = document.getElementById("dropdown-selected");
+const $dropdownList = document.getElementById("dropdown-list");
+const $dropdownArrow = document.getElementById("dropdown-arrow");
+const $dropdownPopularity = document.getElementById("dropdown-list-popularity");
+const $dropdownDate = document.getElementById("dropdown-list-date");
+const $dropdownTitle = document.getElementById("dropdown-list-title");
 
 let sortValue = "popularity";
 
@@ -14,32 +14,32 @@ $dropdownButton.addEventListener("click", (e) => {
   toggleDropdown();
 });
 
-$popularity.addEventListener("click", (e) => {
+$dropdownPopularity.addEventListener("click", (e) => {
   sortValue = "popularity";
-  $selected.innerHTML = "Popularité";
+  $dropdownSelected.innerHTML = "Popularité";
   toggleDropdown();
 });
 
-$date.addEventListener("click", (e) => {
+$dropdownDate.addEventListener("click", (e) => {
   sortValue = "date";
-  $selected.innerHTML = "Date";
+  $dropdownSelected.innerHTML = "Date";
   toggleDropdown();
 });
 
-$title.addEventListener("click", (e) => {
+$dropdownTitle.addEventListener("click", (e) => {
   sortValue = "title";
-  $selected.innerHTML = "Titre";
+  $dropdownSelected.innerHTML = "Titre";
   toggleDropdown();
 });
 
 function toggleDropdown() {
-  if ($list.classList.contains("hide")) {
-    $list.classList.remove("hide");
-    $arrow.innerHTML = "keyboard_arrow_up";
+  if ($dropdownList.classList.contains("hide")) {
+    $dropdownList.classList.remove("hide");
+    $dropdownArrow.innerHTML = "keyboard_arrow_up";
     renderDropdown(sortValue);
   } else {
-    $list.classList.add("hide");
-    $arrow.innerHTML = "keyboard_arrow_down";
+    $dropdownList.classList.add("hide");
+    $dropdownArrow.innerHTML = "keyboard_arrow_down";
     console.log(sortValue);
   }
 }
@@ -47,24 +47,24 @@ function toggleDropdown() {
 function renderDropdown(sortValue) {
   switch (sortValue) {
     case "popularity":
-      $popularity.classList.add("hide");
-      $date.classList.remove("hide");
-      $title.classList.remove("hide");
+      $dropdownPopularity.classList.add("hide");
+      $dropdownDate.classList.remove("hide");
+      $dropdownTitle.classList.remove("hide");
       break;
     case "date":
-      $date.classList.add("hide");
-      $popularity.classList.remove("hide");
-      $title.classList.remove("hide");
+      $dropdownDate.classList.add("hide");
+      $dropdownPopularity.classList.remove("hide");
+      $dropdownTitle.classList.remove("hide");
       break;
     case "title":
-      $title.classList.add("hide");
-      $date.classList.remove("hide");
-      $popularity.classList.remove("hide");
+      $dropdownTitle.classList.add("hide");
+      $dropdownDate.classList.remove("hide");
+      $dropdownPopularity.classList.remove("hide");
       break;
     default:
-      $popularity.classList.add("hide");
-      $date.classList.remove("hide");
-      $title.classList.remove("hide");
+      $dropdownPopularity.classList.add("hide");
+      $dropdownDate.classList.remove("hide");
+      $dropdownTitle.classList.remove("hide");
       break;
   }
 }
--- js/photographers.js
+++ js/photographers.js
@@ -1,39 +1,5 @@
 import data from "../data/data.js";
 
-const id = document.getElementsByTagName("body")[0].getAttribute("data-id");
-const photographer = data.photographers.find((x) => x.id == id);
-const pictures = data.media.filter((x) => x.photographerId == id);
-
-let tagsHTML = "";
-let galleryHTML = "";
-let pictureName = "";
-
-for (let i in photographer.tags) {
-  tagsHTML += `<a href="#" class="tag">#${photographer.tags[i]}</a>`;
-}
-
-for (let j in pictures) {
-  if (pictures[j].image != undefined) {
-    pictureName = splitFileName(pictures[j].image);
-    galleryHTML += `
-    <div class="pic">
-    <img
-      src="../../assets/${photographer.name.split(" ")[0]}/${pictures[j].image}"
-      alt="Portrait AfternoonBreak"
-      class="pic__img"
-    />
-    <div class="pic__text">
-      <p class="pic__title">${pictureName}</p>
-      <p class="pic__price">${pictures[j].price} €</p>
-      <p class="pic__likes">${
-        pictures[j].likes
-      } <i class="fas fa-heart"></i></p>
-    </div>
-  </div>
-    `;
-  }
-}
-
 /* DOM Variables */
 const $name = document.getElementById("name");
 const $location = document.getElementById("location");
@@ -42,26 +8,168 @@ const $tags = document.getElementById("tags");
 const $pp = document.getElementById("pp");
 const $gallery = document.getElementById("gallery");
 
+const $dropdownButton = document.getElementById("dropdown-button");
+const $dropdownSelected = document.getElementById("dropdown-selected");
+const $dropdownList = document.getElementById("dropdown-list");
+const $dropdownArrow = document.getElementById("dropdown-arrow");
+const $dropdownPopularity = document.getElementById("dropdown-list-popularity");
+const $dropdownDate = document.getElementById("dropdown-list-date");
+const $dropdownTitle = document.getElementById("dropdown-list-title");
+
+const id = document.getElementsByTagName("body")[0].getAttribute("data-id");
+const photographer = data.photographers.find((x) => x.id == id);
+const pictures = data.media.filter((x) => x.photographerId == id);
+
+let tagsHTML = "";
+let galleryHTML = "";
+let pictureName = "";
+
+let sortValue = "popularity";
+let picturesSorted = sortPictures(pictures, sortValue);
+
+/* Event Listeners */
+$dropdownButton.addEventListener("click", (e) => {
+  toggleDropdown();
+  renderPictures(picturesSorted);
+});
+
+$dropdownPopularity.addEventListener("click", (e) => {
+  sortValue = "popularity";
+  $dropdownSelected.innerHTML = "Popularité";
+  toggleDropdown();
+  renderPictures(picturesSorted);
+});
+
+$dropdownDate.addEventListener("click", (e) => {
+  sortValue = "date";
+  $dropdownSelected.innerHTML = "Date";
+  toggleDropdown();
+  renderPictures(picturesSorted);
+});
+
+$dropdownTitle.addEventListener("click", (e) => {
+  sortValue = "title";
+  $dropdownSelected.innerHTML = "Titre";
+  toggleDropdown();
+  renderPictures(picturesSorted);
+});
+
+/* Render Tags */
+for (let i in photographer.tags) {
+  tagsHTML += `<a href="#" class="tag">#${photographer.tags[i]}</a>`;
+}
+$tags.innerHTML = tagsHTML;
+
 $name.innerHTML = photographer.name;
 $location.innerHTML = `${photographer.city}, ${photographer.country}`;
 $copyline.innerHTML = photographer.tagline;
-$tags.innerHTML = tagsHTML;
 $pp.setAttribute(
   "src",
   `../../assets/Photographers_ID_Photos/${photographer.portrait}`
 );
-$gallery.innerHTML = galleryHTML;
+
+renderPictures(picturesSorted);
+
+function sortPictures(pictures, sortValue) {
+  switch (sortValue) {
+    case "popularity":
+      return pictures.sort((a, b) => a.likes - b.likes).reverse();
+    case "date":
+      return pictures.sort((a, b) => a.likes - b.likes).reverse();
+    case "title":
+      return pictures.sort(function (a, b) {
+        if (a.image != undefined && b.image != undefined) {
+          let titleA = a.image.toUpperCase();
+          let titleB = b.image.toUpperCase();
+          if (titleA < titleB) {
+            return -1;
+          }
+          if (titleA > titleB) {
+            return 1;
+          }
+
+          // names must be equal
+          return 0;
+        }
+      });
+
+    default:
+      return pictures.sort((a, b) => a.likes - b.likes).reverse();
+  }
+}
 
 function splitFileName(pictureName) {
   pictureName = pictureName.split("_");
-  console.log(pictureName);
   pictureName.shift();
-  console.log(pictureName);
   pictureName = pictureName.join(" ");
-  console.log(pictureName);
   pictureName = pictureName.toString().split(".");
-  console.log(pictureName);
   pictureName.pop();
-  console.log(pictureName);
   return pictureName.join(" ").toString();
+}
+
+function renderPictures(picturesSorted) {
+  console.log(pictures, sortValue);
+  picturesSorted = sortPictures(pictures, sortValue);
+  galleryHTML = "";
+  for (let j in picturesSorted) {
+    if (picturesSorted[j].image != undefined) {
+      pictureName = splitFileName(picturesSorted[j].image);
+      galleryHTML += `
+    <div class="pic">
+    <img
+      src="../../assets/${photographer.name.split(" ")[0]}/${
+        picturesSorted[j].image
+      }"
+      alt="Portrait AfternoonBreak"
+      class="pic__img"
+    />
+    <div class="pic__text">
+      <p class="pic__title">${pictureName}</p>
+      <p class="pic__price">${picturesSorted[j].price} €</p>
+      <p class="pic__likes">${
+        picturesSorted[j].likes
+      } <i class="fas fa-heart"></i></p>
+    </div>
+  </div>
+    `;
+    }
+  }
+  $gallery.innerHTML = galleryHTML;
+}
+
+function toggleDropdown() {
+  if ($dropdownList.classList.contains("hide")) {
+    $dropdownList.classList.remove("hide");
+    $dropdownArrow.innerHTML = "keyboard_arrow_up";
+    renderDropdown(sortValue);
+  } else {
+    $dropdownList.classList.add("hide");
+    $dropdownArrow.innerHTML = "keyboard_arrow_down";
+    console.log(sortValue);
+  }
+}
+
+function renderDropdown(sortValue) {
+  switch (sortValue) {
+    case "popularity":
+      $dropdownPopularity.classList.add("hide");
+      $dropdownDate.classList.remove("hide");
+      $dropdownTitle.classList.remove("hide");
+      break;
+    case "date":
+      $dropdownDate.classList.add("hide");
+      $dropdownPopularity.classList.remove("hide");
+      $dropdownTitle.classList.remove("hide");
+      break;
+    case "title":
+      $dropdownTitle.classList.add("hide");
+      $dropdownDate.classList.remove("hide");
+      $dropdownPopularity.classList.remove("hide");
+      break;
+    default:
+      $dropdownPopularity.classList.add("hide");
+      $dropdownDate.classList.remove("hide");
+      $dropdownTitle.classList.remove("hide");
+      break;
+  }
 }
