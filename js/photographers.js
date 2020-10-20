3a3065f301ab84879bf86ba682bcd6043a51ab79
PabloSKZ <pablo.eskenazi@gmail.Com>
2020-10-20 15:28:33 +0200

    :sparkles: sort by date, :bulb: comments and order code

--- js/photographers.js
+++ js/photographers.js
@@ -1,114 +1,6 @@
 import data from "../data/data.js";
 
-/* DOM Variables */
-const $name = document.getElementById("name");
-const $location = document.getElementById("location");
-const $copyline = document.getElementById("copyline");
-const $tags = document.getElementById("tags");
-const $pp = document.getElementById("pp");
-const $gallery = document.getElementById("gallery");
-
-const $dropdownButton = document.getElementById("dropdown-button");
-const $dropdownSelected = document.getElementById("dropdown-selected");
-const $dropdownList = document.getElementById("dropdown-list");
-const $dropdownArrow = document.getElementById("dropdown-arrow");
-const $dropdownPopularity = document.getElementById("dropdown-list-popularity");
-const $dropdownDate = document.getElementById("dropdown-list-date");
-const $dropdownTitle = document.getElementById("dropdown-list-title");
-
-const id = document.getElementsByTagName("body")[0].getAttribute("data-id");
-const photographer = data.photographers.find((x) => x.id == id);
-const pictures = data.media.filter((x) => x.photographerId == id);
-
-let tagsHTML = "";
-let galleryHTML = "";
-let pictureName = "";
-
-let sortValue = "popularity";
-let picturesSorted = sortPictures(pictures, sortValue);
-
-/* Event Listeners */
-$dropdownButton.addEventListener("click", (e) => {
-  toggleDropdown();
-  renderPictures(picturesSorted);
-});
-
-$dropdownPopularity.addEventListener("click", (e) => {
-  sortValue = "popularity";
-  $dropdownSelected.innerHTML = "Popularité";
-  toggleDropdown();
-  renderPictures(picturesSorted);
-});
-
-$dropdownDate.addEventListener("click", (e) => {
-  sortValue = "date";
-  $dropdownSelected.innerHTML = "Date";
-  toggleDropdown();
-  renderPictures(picturesSorted);
-});
-
-$dropdownTitle.addEventListener("click", (e) => {
-  sortValue = "title";
-  $dropdownSelected.innerHTML = "Titre";
-  toggleDropdown();
-  renderPictures(picturesSorted);
-});
-
-/* Render Tags */
-for (let i in photographer.tags) {
-  tagsHTML += `<a href="#" class="tag">#${photographer.tags[i]}</a>`;
-}
-$tags.innerHTML = tagsHTML;
-
-$name.innerHTML = photographer.name;
-$location.innerHTML = `${photographer.city}, ${photographer.country}`;
-$copyline.innerHTML = photographer.tagline;
-$pp.setAttribute(
-  "src",
-  `../../assets/Photographers_ID_Photos/${photographer.portrait}`
-);
-
-renderPictures(picturesSorted);
-
-function sortPictures(pictures, sortValue) {
-  switch (sortValue) {
-    case "popularity":
-      return pictures.sort((a, b) => a.likes - b.likes).reverse();
-    case "date":
-      return pictures.sort((a, b) => a.likes - b.likes).reverse();
-    case "title":
-      return pictures.sort(function (a, b) {
-        if (a.image != undefined && b.image != undefined) {
-          let titleA = a.image.toUpperCase();
-          let titleB = b.image.toUpperCase();
-          if (titleA < titleB) {
-            return -1;
-          }
-          if (titleA > titleB) {
-            return 1;
-          }
-
-          // names must be equal
-          return 0;
-        }
-      });
-
-    default:
-      return pictures.sort((a, b) => a.likes - b.likes).reverse();
-  }
-}
-
-function splitFileName(pictureName) {
-  pictureName = pictureName.split("_");
-  pictureName.shift();
-  pictureName = pictureName.join(" ");
-  pictureName = pictureName.toString().split(".");
-  pictureName.pop();
-  return pictureName.join(" ").toString();
-}
-
 function renderPictures(picturesSorted) {
-  console.log(pictures, sortValue);
   picturesSorted = sortPictures(pictures, sortValue);
   galleryHTML = "";
   for (let j in picturesSorted) {
@@ -137,6 +29,46 @@ function renderPictures(picturesSorted) {
   $gallery.innerHTML = galleryHTML;
 }
 
+function sortPictures(pictures, sortValue) {
+  switch (sortValue) {
+    case "popularity":
+      return pictures.sort((a, b) => a.likes - b.likes).reverse();
+    case "date":
+      return pictures
+        .sort((a, b) => convertDate(a.date) - convertDate(b.date))
+        .reverse();
+    case "title":
+      return pictures.sort(function (a, b) {
+        if (a.image != undefined && b.image != undefined) {
+          let titleA = a.image.toUpperCase();
+          let titleB = b.image.toUpperCase();
+          if (titleA < titleB) {
+            return -1;
+          }
+          if (titleA > titleB) {
+            return 1;
+          }
+          return 0;
+        }
+      });
+    default:
+      return pictures.sort((a, b) => a.likes - b.likes).reverse();
+  }
+}
+
+function convertDate(date) {
+  return parseInt(date.split("-").join());
+}
+
+function splitFileName(pictureName) {
+  pictureName = pictureName.split("_");
+  pictureName.shift();
+  pictureName = pictureName.join(" ");
+  pictureName = pictureName.toString().split(".");
+  pictureName.pop();
+  return pictureName.join(" ").toString();
+}
+
 function toggleDropdown() {
   if ($dropdownList.classList.contains("hide")) {
     $dropdownList.classList.remove("hide");
@@ -145,7 +77,6 @@ function toggleDropdown() {
   } else {
     $dropdownList.classList.add("hide");
     $dropdownArrow.innerHTML = "keyboard_arrow_down";
-    console.log(sortValue);
   }
 }
 
@@ -173,3 +104,76 @@ function renderDropdown(sortValue) {
       break;
   }
 }
+
+/* DOM Variables */
+const $name = document.getElementById("name");
+const $location = document.getElementById("location");
+const $copyline = document.getElementById("copyline");
+const $tags = document.getElementById("tags");
+const $pp = document.getElementById("pp");
+const $gallery = document.getElementById("gallery");
+
+const $dropdownButton = document.getElementById("dropdown-button");
+const $dropdownSelected = document.getElementById("dropdown-selected");
+const $dropdownList = document.getElementById("dropdown-list");
+const $dropdownArrow = document.getElementById("dropdown-arrow");
+const $dropdownPopularity = document.getElementById("dropdown-list-popularity");
+const $dropdownDate = document.getElementById("dropdown-list-date");
+const $dropdownTitle = document.getElementById("dropdown-list-title");
+
+/* Fetch Data */
+const id = document.getElementsByTagName("body")[0].getAttribute("data-id");
+const photographer = data.photographers.find((x) => x.id == id);
+const pictures = data.media.filter((x) => x.photographerId == id);
+
+/* Initialization */
+let tagsHTML = "";
+let galleryHTML = "";
+let pictureName = "";
+let sortValue = "popularity";
+let picturesSorted = sortPictures(pictures, sortValue);
+
+/* Event Listeners */
+$dropdownButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  toggleDropdown();
+});
+
+$dropdownPopularity.addEventListener("click", (e) => {
+  e.preventDefault();
+  sortValue = "popularity";
+  $dropdownSelected.innerHTML = "Popularité";
+  toggleDropdown();
+  renderPictures(picturesSorted);
+});
+
+$dropdownDate.addEventListener("click", (e) => {
+  e.preventDefault();
+  sortValue = "date";
+  $dropdownSelected.innerHTML = "Date";
+  toggleDropdown();
+  renderPictures(picturesSorted);
+});
+
+$dropdownTitle.addEventListener("click", (e) => {
+  e.preventDefault();
+  sortValue = "title";
+  $dropdownSelected.innerHTML = "Titre";
+  toggleDropdown();
+  renderPictures(picturesSorted);
+});
+
+/* Render Photographer Profile */
+$name.innerHTML = photographer.name;
+$location.innerHTML = `${photographer.city}, ${photographer.country}`;
+$copyline.innerHTML = photographer.tagline;
+$pp.setAttribute(
+  "src",
+  `../../assets/Photographers_ID_Photos/${photographer.portrait}`
+);
+for (let i in photographer.tags) {
+  tagsHTML += `<a href="#" class="tag">#${photographer.tags[i]}</a>`;
+}
+$tags.innerHTML = tagsHTML;
+
+renderPictures(picturesSorted);
